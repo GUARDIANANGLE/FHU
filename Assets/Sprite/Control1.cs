43ac4f29f97ed6c82f5571d2b5edb1d6219dad6b
GUARDIANANGLE <donotuchmehere@gmail.com>
2022-01-04 22:15:58 +0800

    添加了切换视角 跟随鼠标

--- Assets/Sprite/Control1.cs
+++ Assets/Sprite/Control1.cs
@@ -5,33 +5,103 @@ using UnityEngine;
 public class Control1 : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int State;//角色状态
+
+    private int oldState = 0;//前一次角色的状态
+
+    private int UP = 0;//角色状态向前
+
+    private int RIGHT = 1;//角色状态向右
+
+    private int DOWN = 2;//角色状态向后
+
+    private int LEFT = 3;//角色状态向左
+
+    public float speed = 8;
+
     void Start()
+
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
+
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey("w"))
+
         {
-            transform.Translate(0, 0, -0.1f);
+            setState(UP);
+
         }
 
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey("s"))
+
         {
-            transform.Translate(0, 0, 0.1f);
+            setState(DOWN);
+
         }
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey("a"))
+
         {
-            transform.Translate(-0.1f, 0, 0);
+            setState(LEFT);
+
         }
 
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey("d"))
+
         {
-            transform.Translate(0.1f, 0, 0);
+            setState(RIGHT);
+
         }
+
+    }
+
+    void setState(int currState)
+
+    {
+        Vector3 transformValue = new Vector3();//定义平移向量
+
+        int rotateValue = (currState - State) * 90;
+
+        //transform.animation.Play("walk");//播放角色行走动画
+
+        switch (currState)
+
+        {
+            case 0://角色状态向前时，角色不断向前缓慢移动
+
+                transformValue = Vector3.forward * Time.deltaTime * speed;
+
+                break;
+
+            case 1://角色状态向右时。角色不断向右缓慢移动
+
+                transformValue = Vector3.right * Time.deltaTime * speed;
+
+                break;
+
+            case 2://角色状态向后时。角色不断向后缓慢移动
+
+                transformValue = Vector3.back * Time.deltaTime * speed;
+
+                break;
+
+            case 3://角色状态向左时，角色不断向左缓慢移动
+
+                transformValue = Vector3.left * Time.deltaTime * speed;
+
+                break;
+
+        }
+
+        transform.Rotate(Vector3.up, rotateValue);//旋转角色
+
+        transform.Translate(transformValue, Space.World);//平移角色
+
+        oldState = State;//赋值，方便下一次计算
+
+        State = currState;//赋值，方便下一次计算
 
     }
 }
